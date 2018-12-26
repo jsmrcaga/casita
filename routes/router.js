d4e7e43346e4596d054e251959dd4714226051da
Jo Colina <jo.colina@weezevent.com>
2018-12-26 18:26:23 +0100

    Log ITEAD requests to info

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const redfox = require('../global/redfox');
 const router = express.Router();
 const Config = require('../global/config');
 
 // SOnOff main route motherfucker...
 router.get('/', (req, res) => {
+	redfox.info('[WEBSOCKET INFO] New request to server info', req.ip, JSON.stringify(req.headers));
 	return res.json({
 		error: 0,
 		reason: 'ok',
